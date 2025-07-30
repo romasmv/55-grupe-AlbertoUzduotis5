21018c46b6d4b208db5abe4aa99ba9b8451e059e
romasmv <rmorozovas@gmail.com>
2025-07-30 12:34:52 +0300

    up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import Joi from 'joi'
 import express, { json } from "express";
 const app = express();
 
@@ -68,7 +69,48 @@ if(!book) return res.status(404).send('Tokia knyga nerasta ;(')
 
 //POST
 
+// app.post('/api/books',(req,res) => {
+//     const naujaKnyga = {
+//         id: books.length +1,
+//         author: req.body.author,
+//         title: req.body.title
+//     }
+//     books.push(naujaKnyga)
+//     res.send(books)
+// })
+
+
+//----------------------------------------------------------------
+
+
+// app.post('/api/books',(req,res) => {
+//     if(!req.body.author || !req.body.title || req.body.author.length < 5 || req.body.title.length < 1) {
+//       res.status(400).send('Autorius tūri turėti bent 5 simbolius ir pavadinimas tūri turėti bent 1 simbolį');
+//       return;
+//     }
+//     const naujaKnyga = {
+//         id: books.length +1,
+//         author: req.body.author,
+//         title: req.body.title
+//     }
+//     books.push(naujaKnyga)
+//     res.send(books)
+// })
+
+
 app.post('/api/books',(req,res) => {
+    
+  const schema = Joi.object({
+    author: Joi.string().min(5).required(),
+    title: Joi.string().min(1).required(),
+    })
+
+    const validation = schema.validate(req.body)
+
+    if(validation.error) {
+      res.status(400).send(validation.error.details[0].message);
+      return;
+    }
     const naujaKnyga = {
         id: books.length +1,
         author: req.body.author,
@@ -78,6 +120,34 @@ app.post('/api/books',(req,res) => {
     res.send(books)
 })
 
+// PUT 
+
+app.put('/api/books',(req,res) => {
+    const book = books.find((book) => book.id === parseInt(req.params.id)); 
+    if (!book) return res.status(404).send("Tokia knyga nerasta : (");
+    
+  const schema = Joi.object({
+    author: Joi.string().min(5).required(),
+    title: Joi.string().min(1).required(),
+    })
+
+    const validation = schema.validate(req.body)
+
+    if(validation.error) {
+      res.status(400).send(validation.error.details[0].message);
+      return;
+    }
+    // const naujaKnyga = {
+    //     id: books.length +1,
+    //     author: req.body.author,
+    //     title: req.body.title
+    // }
+
+    book.author = req.body.author
+    book.title = req.body.title
+
+    res.send(books)
+})
 
 
 app.listen(3000, ()=> console.log("Listening of port 3000..."));
